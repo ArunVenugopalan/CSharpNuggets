0dc7386045635c0c6694ee85903828368f4baab2
ArunVenugopalan <mail2arooon@gmail.com>
2016-08-23 19:34:19 +1000

    Text file grader
    Sorts the input comma separated file in descending order of grade, last
    name and first name.

---
+++ TextFileGrader/grader/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+// Follows SRP: One purpose per class or method
+namespace grader
+{
+    class Program
+    {
+        // driver method to facilitate unit testing
+        static void RunUnitTest(string path)
+        {
+            if (graderTests.UnitTestgrader.UnitTest1(path))
+            {
+                System.Console.WriteLine("Passes unit test#1");
+            }
+            else
+            {
+                System.Console.WriteLine("Fails unit test#1");
+            }
+        }
+
+        // text file sorter logic
+        static void Main(string[] args)
+        {
+
+            if (args.Length == 0)
+            {
+                System.Console.WriteLine("Please provide input txt file name.");
+                return;
+            }
+
+
+            string filePath = args[0];
+
+            string[] lines = File.ReadAllLines(filePath, Encoding.Default);
+
+            var sorted = lines.Select(line => new
+            {
+                firstName = line.Split(',')[0],
+                lastName = line.Split(',')[1],
+                score = Int32.Parse(line.Split(',')[2]),
+                thisLine = line
+            }
+            ).OrderByDescending(x => x.score).ThenBy(x => x.firstName).ThenBy(x => x.lastName). Select(x => x.thisLine);
+
+            string targetFile = Path.GetFileNameWithoutExtension(filePath) + "-graded.txt";
+            File.WriteAllLines(targetFile, sorted.ToArray(), Encoding.Default);
+
+            System.Console.WriteLine("Output file: {0}", targetFile);
+
+            RunUnitTest(targetFile);
+        }
+    }
+}
---
+++ TextFileGrader/grader/Tests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace graderTests
+{
+    // class to encapsulate all unit test cases
+    class UnitTestgrader
+    {
+        // Sample unit test
+        public static Boolean UnitTest1(string path)
+        {
+            Boolean result = true;
+
+            string[] sorted_sample =
+            {
+                "BUNDY, TERESSA, 88",
+                "KING, MADISON, 88",
+                "SMITH, FRANCIS, 85",
+                "SMITH, ALLAN, 70"
+            };
+
+            string line;
+            int index = 0;
+            System.IO.StreamReader file = new System.IO.StreamReader(path);
+            while ((line = file.ReadLine()) != null)
+            {
+                if (line != sorted_sample[index])
+                {
+                    result = false;
+                    break;
+                }
+                ++index;
+            }
+                
+            return result;
+        }
+    }
+
+    // Add more unit tests here
+}
