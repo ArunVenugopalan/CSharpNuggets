8ad8cbbf22abf23ad4b394d90b3b5e7d1b5e6e13
ArunVenugopalan <mail2arooon@gmail.com>
2016-08-25 14:18:26 +1000

    Refactored to demonstrate SRP and added unit tests

--- TextFileGrader/grader/Program.cs
+++ TextFileGrader/grader/Program.cs
@@ -4,53 +4,80 @@ using System.Linq;
 using System.Text;
 
 // Follows SRP: One purpose per class or method
+
 namespace grader
 {
-    class Program
+
+    // Purpose of this class is to facilitate file IO operations
+
+    // [ Future improvements: class FileIO can be derived from an abstract GenericIO class for future extensibility
+    // The purpose of the GenericIO class is to provide consistent interface irrespective of
+    // the underlying physical/logical infrastructure such as FS, Databases, DFS, Networked IO devices, so on]
+    public class FileIO
     {
-        // driver method to facilitate unit testing
-        static void RunUnitTest(string path)
+        // API to fetch file contents
+        public static string[] GetContents(string filePath)
         {
-            if (graderTests.UnitTestgrader.UnitTest1(path))
-            {
-                System.Console.WriteLine("Passes unit test#1");
-            }
-            else
-            {
-                System.Console.WriteLine("Fails unit test#1");
-            }
+            return File.ReadAllLines(filePath, Encoding.Default);
         }
 
-        // text file sorter logic
-        static void Main(string[] args)
+        // API to write file contents 
+        public static void WriteContents(string filePath, string[] fileContents)
         {
+            File.WriteAllLines(filePath, fileContents, Encoding.Default);
+        }
+    }
 
-            if (args.Length == 0)
-            {
-                System.Console.WriteLine("Please provide input txt file name.");
-                return;
-            }
+    // Purpose of this class is to facilitate sorting
+    // At the moment, this class sort contents by <grade>, <last-name> & <first-name> respectively.
 
-
-            string filePath = args[0];
-
-            string[] lines = File.ReadAllLines(filePath, Encoding.Default);
-
-            var sorted = lines.Select(line => new
+    //[ Future improvements: The order of sorting can be refactored to implement sorting-policy that supports
+    // different sorting orders. The sorting-policy can be implemented using enum and/or by implementing 
+    // dedicated sorting classes using strategy pattern].
+    public class Sorter
+    {
+        // API to sort contents
+        public static string[] SortContents(string[] contents)
+        {
+            var sorted = contents.Select(line => new
             {
                 lastName = line.Split(',')[0],
                 firstName = line.Split(',')[1],
                 score = Int32.Parse(line.Split(',')[2]),
                 thisLine = line
             }
-            ).OrderByDescending(x => x.score).ThenBy(x => x.lastName).ThenBy(x => x.firstName). Select(x => x.thisLine);
+            ).OrderByDescending(x => x.score).ThenBy(x => x.lastName).ThenBy(x => x.firstName).Select(x => x.thisLine);
 
-            string targetFile = Path.GetFileNameWithoutExtension(filePath) + "-graded.txt";
-            File.WriteAllLines(targetFile, sorted.ToArray(), Encoding.Default);
+            return sorted.ToArray();
+        }
 
-            System.Console.WriteLine("Output file: {0}", targetFile);
+        // API to sort input file. By default, it sorts by grade, last-name & first name
+        // The sorting behvaiour can be extended by using enum to specify sort type as second parameter
+        public static string GetSortedFile(string inputFile)
+        {
+            // Get source and target file paths
+            string sourcePath = inputFile;
+            string targetFile = Path.GetFileNameWithoutExtension(sourcePath) + "-graded.txt";
 
-            RunUnitTest(targetFile);
+            string[] fileContents = FileIO.GetContents(sourcePath);
+            string[] fileContentsSorted = SortContents(fileContents);
+            FileIO.WriteContents(targetFile, fileContentsSorted);
+
+            return targetFile;
+        }
+
+        // text file sorter logic
+        static void Main(string[] args)
+        {
+            // Validate inputs before going further
+            if (args.Length == 0)
+            {
+                // Can't process further! Inform user and exit
+                System.Console.WriteLine("Please provide input txt file name.");
+                return;
+            }
+
+            System.Console.WriteLine("Output file: {0}", GetSortedFile(args[0]));
         }
     }
 }
---
+++ TextFileGrader/graderTests/ProgramTests.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+namespace grader.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        // Helper method to provide some test inputs       
+        List<Tuple<string, string>> GetInputs()
+        {
+            return new List<Tuple<string, string>>
+            {   //random contents
+                Tuple.Create( "Tests\\input1.txt", "Tests\\sorted-input1.txt" ),
+                //same contents 
+                Tuple.Create( "Tests\\input2.txt", "Tests\\sorted-input2.txt" ),
+                // Already sorted
+                Tuple.Create( "Tests\\input3.txt", "Tests\\sorted-input3.txt" ),
+                // Empty file
+                Tuple.Create( "Tests\\input4.txt", "Tests\\sorted-input4.txt" ),
+                // ascending order (reverse sorted)
+                Tuple.Create( "Tests\\input5.txt", "Tests\\sorted-input5.txt" )
+            };
+        }
+
+        // Unit test FileIO::GetContents
+        [TestMethod()]
+        public void GetContentsTest()
+        {
+            foreach (var tuple in GetInputs())
+            {
+               string filePath = tuple.Item1;
+               string[] expectedContents = File.ReadAllLines(filePath, Encoding.Default);
+               string[] actualContents = FileIO.GetContents(filePath);
+
+               Assert.IsTrue(expectedContents.SequenceEqual(actualContents),
+                   "GetContents: content mismatch test " + tuple + " failed");
+            }
+        }
+
+        // Unit test FileIO::WriteContents
+        [TestMethod()]
+        public void WriteFileContentsTest()
+        {
+            string[] expectedContents = 
+            {
+                "BUNDY, TERESSA, 88",
+                "KING, MADISON, 88",
+                "SMITH, FRANCIS, 85",
+                "SMITH, ALLAN, 70"
+            };
+
+            string filePath = "sample.txt";
+            FileIO.WriteContents(filePath, expectedContents);
+            string[] actualContents = File.ReadAllLines(filePath, Encoding.Default);
+
+            Assert.IsTrue(expectedContents.SequenceEqual(actualContents), 
+                "WriteContents: content mismatch test failed");
+        }
+
+        // Unit test Sorter::SortContents
+        [TestMethod()]
+        public void SortFileTest()
+        {
+            foreach (var tuple in GetInputs())
+            {
+                string[] expectedContents = File.ReadAllLines(tuple.Item2, Encoding.Default);
+                string[] actualContents = Sorter.SortContents(File.ReadAllLines(tuple.Item1, Encoding.Default));
+
+                Assert.IsTrue(expectedContents.SequenceEqual(actualContents),
+                    "SortContents: Content is not sorted as per requirements " + tuple);
+            }
+        }
+
+        // Unit test Sorter::GetSortedFile
+        [TestMethod()]
+        public void GetSortedFileTest()
+        {
+            foreach (var tuple in GetInputs())
+            {
+                string filePath = tuple.Item1;
+                string sortedfileForTest = tuple.Item2;
+                string expectedFileName = Path.GetFileNameWithoutExtension(tuple.Item1) + "-graded.txt";
+
+                string[] expectedContents = File.ReadAllLines(sortedfileForTest, Encoding.Default);
+                string actualfileName = Sorter.GetSortedFile(filePath);
+                string[] actualContents = File.ReadAllLines(actualfileName, Encoding.Default);
+
+                Assert.IsTrue(expectedFileName.Equals(actualfileName),
+                    "GetSortedFile: Output file name isn't named as per requirements");
+                Assert.IsTrue(expectedContents.SequenceEqual(actualContents),
+                    "GetSortedFile: Sorting test failed");
+            }
+        }
+    }
+}
